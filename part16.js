a67d2095d2486164b4b677127dcba55fbdfc59e8
Osiris8 <miganosiris8@gmaaail.com>
2022-12-09 08:05:04 +0100

    Variables in JSX

---
+++ part16.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const theBestString = "tralalalala i am da best";
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById("app"));
+
+/* Outputs
+images/part16.png
+*/
