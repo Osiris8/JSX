5f62a2f9c1a327ce70bacce71ef6d591c0c459e4
Osiris8 <miganosiris8@gmaaail.com>
2022-12-09 08:10:50 +0100

    Variable Attributes in JSX

---
+++ part17.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const goose =
+  "https://content.codecademy.com/courses/React/react_photo-goose.jpg";
+
+// Declare new variable here:
+const gooseImg = <img src={goose} />;
+
+ReactDOM.render(gooseImg, document.getElementById("app"));
+
+/* Outputs
+images/part17.png
+*/
