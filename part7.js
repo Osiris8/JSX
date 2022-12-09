08af1a8bc5edea4cc0fc474f4fe8130b74a0b2f6
Osiris8 <miganosiris8@gmaaail.com>
2022-12-09 07:32:40 +0100

    Rendering JSX

---
+++ part7.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Copy code here:
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+
+/* Outputs
+images/part7.png
+*/
