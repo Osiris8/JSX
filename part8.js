969ea115d11831e06fc56c7072911e32f5b476f8
Osiris8 <miganosiris8@gmaaail.com>
2022-12-09 07:35:33 +0100

    ReactDOM.render() I

---
+++ part8.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById("app"));
+
+/* Outputs
+images/part8.png
+*/
