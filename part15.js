9e5c9ca9bcb96d5d461cb59ec617d9a968f959db
Osiris8 <miganosiris8@gmaaail.com>
2022-12-09 08:02:56 +0100

    20 Digits of Pi in JSX

---
+++ part15.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const pi = (
+  <div>
+    <h1>PI, YALL!</h1>
+    <p>{Math.PI.toFixed(20)}</p>
+  </div>
+);
+
+ReactDOM.render(pi, document.getElementById("app"));
+
+/* Outputs
+images/part15.png
+*/
